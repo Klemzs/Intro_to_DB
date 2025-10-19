d5df87eab3b59d172c1dbaf49052d1121aef4a6b
Clement Odeh <odehclementonwu@gmail.com>
2025-10-19 23:41:01 +0100

    add task 5

--- task_5.sql
+++ task_5.sql
@@ -1,23 +1,27 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Jesuslord4545?"
+    host="localhost",
+    user="root",
+    password="Jesuslord4545?",
+    database="alx_book_store"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("USE alx_book_store")
+# Delete the specific customer that's causing conflict
+mycursor.execute("DELETE FROM customers WHERE customer_id = 1")
 
-customer_data = "INSERT INTO Customers (customer_id, customer_name, email, address)  VALUES (%s, %s, %s, %s)"
-customer_value = (1,"Cole Baidoo", "cbaidoo@sandtech.com","123 Happiness Ave." )
+print(f"Deleted {mycursor.rowcount} conflicting customers")
+
+# Now insert the new customer
+customer_data = "INSERT INTO customers (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
+customer_value = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
 
 mycursor.execute(customer_data, customer_value)
-
 mydb.commit()
 
-print(mycursor.rowcount)
+print(f"Customer inserted with ID: {customer_value[0]}")
 
 mycursor.close()
 mydb.close()
