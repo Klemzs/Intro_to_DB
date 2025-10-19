c1f30a8537ccadd1dd8e1e214729a04a3ce6bb6c
Clement Odeh <odehclementonwu@gmail.com>
2025-10-19 20:30:24 +0100

    add task 3

--- task_3.sql
+++ task_3.sql
@@ -3,12 +3,12 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "Jesuslord4545?",
-    database = "alx_book_store"
+    password = "Jesuslord4545?"
 )
 
 mycursor = mydb.cursor()
 
+mycursor.execute("USE alx_book_store")
 mycursor.execute("SHOW TABLES")
 
 tables = mycursor.fetchall()
