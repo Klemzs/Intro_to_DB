608100ca3fee964259e15c989ef5030fd9d293c6
Clement Odeh <odehclementonwu@gmail.com>
2025-10-19 23:55:56 +0100

    add task 6

--- task_6.sql
+++ task_6.sql
@@ -9,11 +9,16 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+delete_query = "DELETE FROM customers WHERE customer_id IN (2, 3, 4)"
+mycursor.execute(delete_query)
+deleted_count = mycursor.rowcount
+print(f"Deleted {deleted_count} existing customers")
+
 customer_data = "INSERT INTO customers (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
 customer_values = [
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
+    (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+    (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+    (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
 ]
 
 mycursor.executemany(customer_data, customer_values)
