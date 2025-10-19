38ec846b2849de5d2349ebbb14fd44fed71c35cb
Clement Odeh <odehclementonwu@gmail.com>
2025-10-19 23:17:25 +0100

    add alx bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -15,14 +15,14 @@ CREATE DATABASE IF NOT EXISTS alx_book_store
 
 mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Authors(
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215)                
 )
 """)
 
 mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Books(
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Books(
 
 mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Customers(
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT            
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS Customers(
 
 mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Orders(
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS Orders(
 
 mycursor.execute("""
 CREATE TABLE IF NOT EXISTS Order_Details(
-    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    orderdetail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
