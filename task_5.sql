5b3c1de6c08c0e5784e27aea3ec9d6579ade7423
Clement Odeh <odehclementonwu@gmail.com>
2025-10-19 22:39:46 +0100

    add task 5

--- task_5.sql
+++ task_5.sql
@@ -10,7 +10,7 @@ mycursor = mydb.cursor()
 
 mycursor.execute("USE alx_book_store")
 
-customer_data = "INSERT INTO Customers (customer_id, customer_name, email, address)  VALUES (%s, %s, %s, %s)"
+customer_data = "INSERT IGNORE INTO Customers (customer_id, customer_name, email, address)  VALUES (%s, %s, %s, %s)"
 customer_value = (1,"Cole Baidoo", "cbaidoo@sandtech.com","123 Happiness Ave." )
 
 mycursor.execute(customer_data, customer_value)
